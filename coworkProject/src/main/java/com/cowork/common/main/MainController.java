bde2b2de76c5957b0722039ffb413f26d70cdf14
peb7544 <peb7544@naver.com>
2024-05-28 16:22:22 +0900

    240528_peb_board

---
+++ coworkProject/src/main/java/com/cowork/common/main/MainController.java
@@ -0,0 +1,15 @@
+package com.cowork.common.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String mainPage() {
+		
+		return "common/main"; 
+	}
+
+}
