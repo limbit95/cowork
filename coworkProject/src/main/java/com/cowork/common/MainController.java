c9b575bd5e0ee3b7580447d9618838898b965e18
vyoonv <vyoonheejv@gmail.com>
2024-05-28 10:38:04 +0900

    header

---
+++ coworkProject/src/main/java/com/cowork/common/MainController.java
@@ -0,0 +1,14 @@
+package com.cowork.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	public String mainPage() {
+		
+		return "common/main"; 
+	}
+
+}
