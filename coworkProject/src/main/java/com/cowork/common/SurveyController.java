fa9ff65cdea06c9b7e3a51757c42b18edc706843
songjiyoon9507 <songjiyoon9507@gmail.com>
2024-05-28 16:30:06 +0900

    metadata test

--- coworkProject/src/main/java/com/cowork/common/SurveyController.java
+++ coworkProject/src/main/java/com/cowork/common/SurveyController.java
@@ -3,11 +3,15 @@ package com.cowork.common;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class SurveyController {
 
 	@GetMapping("survey")
 	public String servey() {
+		log.info("log");
 		return "survey/surveyList";
 	}
 }
