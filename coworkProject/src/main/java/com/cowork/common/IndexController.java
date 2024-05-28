da667913dcf22f22a8fa3b24c4424c65a353f2c0
vyoonv <vyoonheejv@gmail.com>
2024-05-28 16:07:34 +0900

    0528_jyh_common_signup_login

--- coworkProject/src/main/java/com/cowork/common/IndexController.java
+++ coworkProject/src/main/java/com/cowork/common/IndexController.java
@@ -1,12 +1,12 @@
 package com.cowork.common;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-	@RequestMapping("/")
+	@GetMapping("common/index")
 	public String indexPage() {
 		
 		return "common/index";
