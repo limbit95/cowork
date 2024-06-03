a5c71b40e346cc0a5bc9acdbd78a42a8cd91e29e
vyoonv <vyoonheejv@gmail.com>
2024-06-03 17:22:14 +0900

    0603_jyh_todo

---
+++ coworkProject/src/main/java/com/cowork/employee/todo/model/dto/Todo.java
@@ -0,0 +1,22 @@
+package com.cowork.employee.todo.model.dto;
+
+public class Todo {
+
+	private int todoNo; 
+	private String todoTitle; 
+	private String todoContent; 
+	private String todoWriteDate; 
+	private String todoEndDate; 
+	private String todoComplete; 
+	private int empCode;
+	
+	private int todoMgrNo; 
+	
+	private int fileNo; 
+	private String filePath; 
+	private String fileOriginName; 
+	private String fileRename; 
+	private String fileUploadDate; 
+	private int fileOrder; 
+	
+}
