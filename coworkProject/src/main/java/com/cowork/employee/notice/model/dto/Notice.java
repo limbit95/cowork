54ee1a5c68c1c80b488c734de23b872f75a7b476
peb7544 <peb7544@naver.com>
2024-06-03 16:08:07 +0900

    0603_peb_notice

---
+++ coworkProject/src/main/java/com/cowork/employee/notice/model/dto/Notice.java
@@ -0,0 +1,26 @@
+package com.cowork.employee.notice.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Notice {
+	
+	// Notice 테이블 컬럼
+	private int noticeNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private String noticeWriteDate;
+	private String noticeUpdateDate;
+	private String noticeDelFl;
+	private int empCode;
+}
