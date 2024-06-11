119f3c1cef1331b00dee302f010d9fa067ca9dd5
chlwowns <wowns590@naver.com>
2024-06-11 10:04:09 +0900

    0611_cjj_survey js

--- coworkProject/src/main/java/com/cowork/employee/survey/controller/SurveyController.java
+++ coworkProject/src/main/java/com/cowork/employee/survey/controller/SurveyController.java
@@ -2,7 +2,12 @@ package com.cowork.employee.survey.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cowork.employee.survey.model.dto.SurveyData;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,6 +32,19 @@ public class SurveyController {
 	public String serveyInsert() {
 		return "employee/survey/surveyInsert";
 	}
+	
+	
+	
+   @PostMapping("/survey2")
+   @ResponseBody
+    public SurveyData handleSurvey(@RequestBody SurveyData surveyData) {
+        // 여기서 surveyData를 처리합니다.
+        // 예를 들어, surveyData의 내용을 로그로 출력할 수 있습니다.
+        System.out.println("Received Survey Data: " + surveyData);
+
+        // 처리 후 응답 반환 (여기서는 간단히 입력 데이터를 그대로 반환)
+        return surveyData;
+   }
 	
 	
 	
