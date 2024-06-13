00c0c7f9467a6d5ae6d625b8dbe6abdb52018bb9
vyoonv <vyoonheejv@gmail.com>
2024-06-13 17:04:29 +0900

    0613_jyh_mail하는중

--- coworkProject/src/main/java/com/cowork/employee/mail/model/mapper/MailMapper.java
+++ coworkProject/src/main/java/com/cowork/employee/mail/model/mapper/MailMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
 
 import com.cowork.employee.mail.model.dto.Mail;
 import com.cowork.employee.mail.model.dto.MailFile;
@@ -12,10 +13,11 @@ import com.cowork.employee.mail.model.dto.MailFile;
 public interface MailMapper {
 
 	/** 전체 메일함 조회 
-	 * @param empCode
+	 * @param paramMap
+	 * @param rowBounds 
 	 * @return
 	 */
-	List<Mail> mailList(int empCode);
+	List<Mail> mailList(Map<String, Object> paramMap, RowBounds rowBounds);
 
 	/** 메일 상세 조회 
 	 * @param mailNo
@@ -24,10 +26,16 @@ public interface MailMapper {
 	Mail mailDetail(int mailNo);
 
 	/** 전체 메일 개수 조회 
-	 * @param empCode
+	 * @param paramMap
 	 * @return
 	 */
 	int mailCount(int empCode);
+	
+	/** 전체 메일 리스트 개수 조회 
+	 * @param paramMap
+	 * @return
+	 */
+	int listCount(Map<String, Object> paramMap);
 
 	/** 안 읽은 메일 개수 조회 
 	 * @param empCode
@@ -46,5 +54,61 @@ public interface MailMapper {
 	 * @return
 	 */
 	List<MailFile> fileList(Map<String, Object> fileMap);
+
+	/** 보낸 메일 리스트 조회 
+	 * @param paramMap
+	 * @param rowBounds 
+	 * @return
+	 */
+	List<Mail> sentMailList(Map<String, Object> paramMap, RowBounds rowBounds);
+
+	/** 보낸 메일함 전체 개수 조회 
+	 * @param empCode
+	 * @return
+	 */
+	int sentMailCount(int empCode);
+	
+	/** 보낸 메일함 안 읽은 메일 개수 
+	 * @param empCode
+	 * @return
+	 */
+	int sentMailNoReadCount(int empCode);
+	
+	/** 페이지네이션용 보낸메일함 전체 메일개수 조회 
+	 * @param paramMap
+	 * @return
+	 */
+	int sentListCount(Map<String, Object> paramMap);
+
+	/** 받은 메일함 조회
+	 * @param paramMap
+	 * @param rowBounds
+	 * @return
+	 */
+	List<Mail> inMailList(Map<String, Object> paramMap, RowBounds rowBounds);
+
+	/** 받은 메일함 전체 개수 
+	 * @param empCode
+	 * @return
+	 */
+	int inMailCount(int empCode);
+
+	/** 받은 메일함 안 읽은 메일 개수 
+	 * @param empCode
+	 * @return
+	 */
+	int inMailNoReadCount(int empCode);
+
+	/** 페이지네이션용 받은 메일함 리스트 조회 
+	 * @param paramMap
+	 * @return
+	 */
+	int inListCount(Map<String, Object> paramMap);
+
+	
+
+	
+
+	
 
 }
