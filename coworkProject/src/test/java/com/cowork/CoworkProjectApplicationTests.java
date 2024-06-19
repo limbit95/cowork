8903972259bdbccc1de39ad42b21ac5b96ac4b1b
limbit95@gmail.com <limbit95@gmail.com>
2024-06-19 18:34:42 +0900

    test

--- coworkProject/src/test/java/com/cowork/CoworkProjectApplicationTests.java
+++ coworkProject/src/test/java/com/cowork/CoworkProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.cowork;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CoworkProjectApplicationTests {
 
 	@Test
